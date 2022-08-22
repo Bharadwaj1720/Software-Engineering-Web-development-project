76794d159a65b479d8db9a7226e94f1a8b1a9ce8
Vishnu767 <vishnuvardhanpuppala2003@gmail.com>
2022-08-22 16:55:46 +0530

    Using buttons component

--- FrontEnd/src/User_Components/Main.js
+++ FrontEnd/src/User_Components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Header from './Header'
+import Buttons from './Buttons'
 export default function Main(){
     return(
         <div>
@@ -8,12 +9,8 @@ export default function Main(){
                 title="IIT TIRUPATI ERP"
             />
             <div className="container-fluid h-100 text-dark bg-light d-block">
-                <div className="row justify-content-center">
-                    <Link className="col-6 col-md-3 btn btn-info m-2 text-light" to="health-center">Health Center</Link>
-                </div>
-                <div className="row justify-content-center">
-                    <Link className="col-6 col-md-3 btn btn-info m-2 text-light" to="library">Library</Link>
-                </div>
+                <Buttons title="Health Center" path="health-center" />
+                <Buttons title="Library" path="library" />
             </div>
         </div>
     );
