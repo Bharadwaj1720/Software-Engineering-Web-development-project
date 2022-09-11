6da7e4bb189666fa15c9013aec61433f25ee2be4
Bharadwaj Vuppala <vuppalabharadwaj172002@gmail.com>
2022-09-11 22:30:42 +0530

    updated

--- Details.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Details(props){
-    return(
-        <span className='border p-2'>
-            <div className="detail-header">{props.title}</div>
-            <input className="name-bar" type="text" placeholder={props.description}></input>     
-        </span>
-    );
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Software-Engineering-Web-development-project
