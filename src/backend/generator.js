abe37b298ddf1f4841623bf47569bfff8ed91c4f
Mathew K J <79681351+MathewKJ2048@users.noreply.github.com>
2022-09-14 15:37:39 +0530

    Add files via upload

--- src/backend/generator.js
+++ src/backend/generator.js
@@ -1,5 +1,6 @@
 
 function random(p1, p2) {
+    if(p1===p2)return p1;
     return p1 + (Math.floor(Math.random()*10000) % (p2-p1));   // The function returns a random number between p1 and p2
 }
 
@@ -17,7 +18,10 @@ function random_name(length)
     name = name.charAt(0).toLocaleUpperCase().concat(name.substring(1))
     return name
 }
-
+function random_personal_name()
+{
+    return random_name(random(5,10)).concat(" ").concat(random_name(random(5,10)));
+}
 function random_roll_number()
 {
     const branches = ["CS","ME","EE","CH","CE","MA","PH","CY","HS"]
@@ -27,6 +31,53 @@ function random_roll_number()
     return branches[random(0,9)].concat(years[random(0,5)]).concat(study[random(0,3)]).concat(num)
 }
 
-module.exports = {
-    random,random_name,random_roll_number
+function random_book_name()
+{
+    const starts = ["Beginners guide to","The Art of","Modern theory of","Principles of","Introduction to","Design and Analysis of"]
+    const endings = ["- a Complete Guide", "for Dummies","- a Comprehensive Approach","- a Modern Approach"," Simplified"]
+    var name = random_name(random(4,12));
+    if(random(0,10)%2==0)name = name.concat(" ").concat(endings[random(0,endings.length)]);
+    else name = starts[random(0,starts.length)].concat(" ").concat(name);
+    return name;
+}
+
+function random_ISBN_number()
+{
+    var number = "";
+    var sum=0;
+    for(var i=0;i<9;i++)
+    {
+        var num = random(0,10);
+        number=number.concat(num);
+        sum+=(i+1)*num;
+    }
+    sum = (11-sum%11)%11;
+    if(sum===10)
+    {
+        const prev_num = number.charAt(number.length-1)-'0';
+        number = number.substring(0,9);
+        if(prev_num===9)
+        {
+            number.concat("8");
+            sum = 0;
+        }
+        else
+        {
+            let s = prev_num+1
+            number.concat(s);
+            sum = 9;
+        }
+    }
+    number=number.concat(sum);
+    return number;
+}
+
+module.exports = 
+{
+    random,
+    random_name,
+    random_roll_number, 
+    random_personal_name, 
+    random_book_name, 
+    random_ISBN_number
 };
