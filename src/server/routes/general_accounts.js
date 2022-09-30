a5043563dee92a66a595a58da0eee94db31b2073
Mathew K J <79681351+MathewKJ2048@users.noreply.github.com>
2022-09-30 11:07:57 +0530

    Add files via upload

--- src/server/routes/general_accounts.js
+++ src/server/routes/general_accounts.js
@@ -1,9 +1,3 @@
-/*TODO
-move add account to account manager, check to make sure usernames do not clash
-add routes for change password and forgot password
-incase of forgot password, new password is emailed and backend changes the password
-*/
-
 
 const password_module = require("../security")
 const express = require("express");
@@ -11,12 +5,23 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const { ObjectID } = require("bson");
 const ObjectId = require("mongodb").ObjectId;
- 
-const allowed_info = {"_id":1,"username":1,"status":1,"password":0}
 
-// returns every username, status and _id of all accounts
+
+
+
+
+
+
+const allowed_info = {"_id":0,"username":1,"status":1,"password":0}
+// returns username, status of all accounts
 recordRoutes.route("/general_accounts").post(function (req, res) {
- let db_connect = dbo.getDb();
+  /*
+  req: auth_token
+  */
+ function callback(is_valid,message) {
+  if(!is_valid){res.json("authentication failed:"+message);return;}
+  //
+  let db_connect = dbo.getDb();
  db_connect
    .collection("general_accounts")
    .find({},allowed_info)
@@ -25,31 +30,96 @@ recordRoutes.route("/general_accounts").post(function (req, res) {
      res.json(result);
      console.log("all general_accounts sent")
    });
+   //
+  }
+   password_module.check_authentication(req.body.auth_token,["root"],callback)
 });
 
+
+
+
+
 recordRoutes.route("/general_accounts/add").post(function (req, response) {
+    /*
+    req: username, password_plaintext, status
+    */
+    function callback(is_valid,message) {
+      if(!is_valid){res.json("authentication failed:"+message);return;}
     let db_connect = dbo.getDb();
+    if(req.body.status==="root")
+    {
+      console.log("attempt to create root account")
+      response.json("error: root accounts cannot be created")
+      return;
+    }
     let myobj = {
       username: req.body.username,
       status: req.body.status,
       password: password_module.get_hash_and_salt(req.body.password_plaintext)
     }
-    db_connect.collection("general_accounts").insertOne(myobj, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-      console.log("account added: "+req.body.username)
-    });
+    db_connect.collection("general_accounts").find({username:req.body.username}).toArray(function(err,res){
+      if(err)throw err;
+      if(res.length>0)
+      {
+        response.json("account with given username already exists");
+        console.log("creation failed due to duplicate username: "+myobj.username);
+        return;
+      }
+      db_connect.collection("general_accounts").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        response.json(res);
+        console.log("account added: "+req.body.username)
+      });
+    })
+      }
+    password_module.check_authentication(req.body.auth_token,["root"],callback)
+
    });
 
+
+
+
+   recordRoutes.route("/general_accounts/delete").post((req, response) => {
+    /*
+    req: auth_token, username
+    */
+    function callback(is_valid,message) {
+      if(!is_valid){res.json("authentication failed:"+message);return;}
+      //
+      let db_connect = dbo.getDb();
+    let myquery = { username: req.body.username };
+    db_connect.collection("general_accounts").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      if(result === null){console.log("given username does not exist");
+      response("non-existent account");return;}
+      if(result.status === "root"){console.log("attempt to delete root");
+      response("gived account is root and cannot be deleted");return;}
+      db_connect.collection("general_accounts").deleteOne(myquery, function (err, obj) {
+        if (err) throw err;
+        console.log("account deleted: "+req.body.username);
+        response.json(obj);
+      });
+    });}
+    password_module.check_authentication(req.body.auth_token,["root"],callback)
+   });
+
+
+
+
+
+
 recordRoutes.route("/general_accounts/login").post(function (req, response) {
-  
+  /*
+  req: username, password_plaintext, device_id
+  */
   let db_connect = dbo.getDb();
   let myquery = { username: req.body.username };
   db_connect
     .collection("general_accounts")
     .findOne(myquery, function (err, result) {
       if (err) throw err;
-      if(password_module.check_equality(req.body.password_plaintext,result.password.hash,result.password.salt))
+      if(password_module
+        .check_equality(req.body.password_plaintext,result.password.hash,result.password.salt))
       {
          const auth = password_module.generate_auth_token();
          const ahs = password_module.get_hash_and_salt(auth);
@@ -62,7 +132,7 @@ recordRoutes.route("/general_accounts/login").post(function (req, response) {
         }
         db_connect.collection("authentication_tokens").insertOne(myobj, function (err, res) {
         if (err) throw err;
-        response.json({auth_token:auth});
+        response.json({auth_token:auth,account:result});
         });
         console.log(req.body.username+" has logged in");
         //
@@ -71,7 +141,15 @@ recordRoutes.route("/general_accounts/login").post(function (req, response) {
     });
  });
 
+
+
+
+
+
  recordRoutes.route("/general_accounts/logout").post(function (req, res) {
+  /*
+  req: username, device_id (may be null for logout of all devices)
+  */
   let db_connect = dbo.getDb();
   let all_devices = req.body.device_id === null;
   console.log(all_devices)
@@ -86,19 +164,89 @@ recordRoutes.route("/general_accounts/login").post(function (req, response) {
       console.log(message);
       res.json(message);
     });
-      
+ });
+
+
+
+
+recordRoutes.route("/general_accounts/edit").post(function(req,res){
+  /*
+  req: username, new_status
+  */
+  function callback(is_valid,message) {
+    if(!is_valid){res.json("authentication failed:"+message);return;}
+    if(req.body.new_status === "root")
+    {
+      console.log("attempt to upgrade to root")
+      res.json("error: ony one root may exist")
+      return;
+    }
+    let db_connect = dbo.getDb();
+    db_connect.collection("general_accounts").updateOne({username:req.body.username},
+      {$set:{status:req.body.new_status}},function(err,result){
+          if(err)throw err;
+          res.json(result);
+      })
+  }
+  password_module.check_authentication(req.body.auth_token,["root"],callback)
+})
+
+
+
+
+
+
+recordRoutes.route("general_accounts/force_logout").post(function(req,res){
+  /*
+  req: auth_token
+  */
+  function callback(is_valid,message) {
+    if(!is_valid){res.json("authentication failed:"+message);return;}
+    password_module.force_logout();
+  }
+  password_module.check_authentication(req.body.auth_token,["root"],callback)
+})
+
+
+
+
+
+ recordRoutes.route("/general_accounts/change_password").post(function (req, res) {
+  /*
+  req:  unsername, password_plaintext/auth_token, new_password_plaintext
+  */
+  let db_connect = dbo.getDb();
+  let myquery = {username: req.body.username}
+  db_connect
+    .collection("general_accounts").findOne(myquery,function(err,result){
+      if(err)throw err
+      let message = ""
+      function action() {
+        db_connect.collection("general_accounts")
+          .updateOne({username:result.username},
+            {$set:{password:password_module.get_hash_and_salt(req.body.new_password_plaintext)}}
+            ,function(err,result){
+            if(err)throw err
+            res.json(result)
+          })
+      }
+      if(result === null )message="username does not exist"
+      if(typeof req.password_plaintext != "undefined")
+      { 
+        if(password_module.check_equality(req.password_plaintext,result.password.hash,result.password.salt))
+        {
+           console.log("password changed by user")
+           action();
+        } 
+      }
+      else if(typeof req.auth_token != "undefined")
+      {
+          password_module.check_authentication(req.auth_token,["root"],function(is_valid,message){
+          console.log("force change of password by root")
+          action();
+        })
+      }
+    });
  });
 
 module.exports = recordRoutes;
-/*
-general_accounts:
-
-username
-password: hash salt
-status
-
-authentication_tokens:
-
-account_id
-token: hash salt
-*/
--- src/server/routes/medical_drugs.js
+++ src/server/routes/medical_drugs.js
@@ -2,10 +2,13 @@ const express = require("express");
 const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
+
+const security = require("../security.js");
  
  
 // This section will help you get a list of all the records.
 recordRoutes.route("/medical_drugs").get(function (req, res) {
+  function callback(is_valid,message){
  let db_connect = dbo.getDb();
  db_connect
    .collection("medical_drugs")
@@ -13,5 +16,6 @@ recordRoutes.route("/medical_drugs").get(function (req, res) {
    .toArray(function (err, result) {
      if (err) throw err;
      res.json(result);
-   });
+   });}
+   security.check_authentication(req.body.auth_token,["all","medical"],callback)
 });
