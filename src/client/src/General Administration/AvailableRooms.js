58ac79c8c244044b122300a6e8be42de8c7559ec
Vishnu767 <88651403+Vishnu767@users.noreply.github.com>
2022-10-31 00:10:08 +0530

    Change in Available rooms file

--- src/client/src/General Administration/AvailableRooms.js
+++ src/client/src/General Administration/AvailableRooms.js
@@ -1,53 +1,70 @@
-import React from "react";
-import Layout from "../Layout";
-export default function AvailableRooms() {
+import React, {useState, useEffect} from "react";
+import Header from './Header'
+
+const Room = (props) => (
+    <tr>
+      <td>{props.record.sno}</td>
+      <td>{props.record.roomno}</td>
+      <td>{props.record.type}</td>
+      <td>{props.record.capacity}</td>
+      <td>{props.record.floor}</td>
+    </tr>
+);
+
+export default function AvailableRooms(){
+    const [rooms, setRooms] = useState([]);
+    useEffect(() => {
+        async function getRooms() {
+          const response = await fetch(`http://localhost:5000/`);
+      
+          if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            window.alert(message);
+            return;
+          }
+      
+          const records = await response.json();
+          console.log(records.name)
+          setRooms(records.name);
+        }
+      
+        getRooms();
+      
+        return;
+      }, [rooms.length]);
+
+      function roomList() {
+        return rooms.map((room) => {
+          return (
+            <Room
+              room={room}
+              //deleteRecord={() => deleteRecord(record._id)}
+              key={room._id}
+            />
+          );
+        });
+      }
+
     return (
         <div>
-            <Layout message="Available Rooms" />
-            <div class="p-3" style={{
-                marginTop: 100,
-                marginLeft: 60
-            }}>
-                <table class="table table-hover table-bordered">
-                    <thead>
+            <Header title="Medical Records"/>
+            <div className="container px-5 py-1">
+                <table className="table w-100">
+                    <thead className="bg-dark text-white">
                         <tr>
-                            <th scope="col">S No.</th>
+                            <th scope="col">#</th>
                             <th scope="col">Room No.</th>
-                            <th scope="col">Room Type</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Capacity</th>
                             <th scope="col">Floor</th>
-                            <th scope="col">Amount per day</th>
-
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>102</td>
-                            <td>3 BHK</td>
-                            <td>1st Floor</td>
-                            <td>₹800</td>
-
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>202</td>
-                            <td>3 BHK</td>
-                            <td>2nd Floor</td>
-                            <td>₹800</td>
-
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>203</td>
-                            <td>2 BHK</td>
-                            <td>2nd Floor</td>
-                            <td>₹600</td>
-
-                        </tr>
+                    <tbody className="tbody-light">
+                        {roomList()}
                     </tbody>
                 </table>
             </div>
         </div>
-
+        
     );
 }
