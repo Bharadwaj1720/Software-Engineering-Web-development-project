2d36b8892821f43e5b1eaa1611ca0251731bb967
Vishnu767 <88651403+Vishnu767@users.noreply.github.com>
2022-10-30 12:13:04 +0530

    Final working version of Appointment.js

--- src/server/routes/appointment.js
+++ src/server/routes/appointment.js
@@ -1,9 +1,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 //const uri = "mongodb+srv://Bootstrapparadox:Team09@cluster0.pinftg3.mongodb.net/exampledb";
-mongoose.connect("mongodb+srv://Bootstrapparadox:ASMKJPVVUKVB@cluster0.pinftg3.mongodb.net/bin_db",{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.pinftg3.mongodb.net/bin_db",{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
+app.use(cors());
 
 const appointmentschema = new mongoose.Schema({
     ID:{
@@ -25,9 +27,6 @@ const appointmentschema = new mongoose.Schema({
         type:String,
     },
     time:{
-        type:String,
-    },
-    visit:{
         type:String,
     },
     cause:{
@@ -52,7 +51,7 @@ function random(length)
     for(var i=0; i<length ;i++)result=result+character[Math.floor(Math.random()*character.length)];
     return result;
 }
-function doctor(date,time)
+function doctor(date,time1)
 {
     const doctor=['kushwanth','vishnu','vardhan','mathew','abhijeeth','bharadwaj']
     let l=0;
@@ -61,22 +60,23 @@ function doctor(date,time)
     const day = (date2.getTime() - date1.getTime())/1000*60*60*24;
     if(day%7<5&&day%7>0)l=0;
     else l=3;
-    if(time[5]=='a'&&time[0]<'8')return doctor[l];
-    if(time[5]=='a'&&time[0]>'8')return doctor[l+1];
-    if(time[5]=='p'&&time[0]<'4')return doctor[l+1];
-    if(time[5]=='p'&&time[0]>'4')return doctor[l+2];
+    let var1=time1[0]+time1[1]
+    if(var1<"08")return doctor[l];
+    else if("08"<var1<"16")return doctor[l+1];
+    else if("16"<var1<"24")return doctor[l+1];
+    
 
     return '';
 }
 
-app.get('/new',function(req,res){
+app.post('/new',function(req,res){
+    console.log("Attempt to access new");
     const appointment1=new Appointment({
         ID:random(6),
         date:req.body.date,
-        doctor:doctor(req.body.date,req.body.time),
         time:req.body.time,
-        visit:req.body.visit,
-        cause:req.body.cause
+        cause:req.body.cause,
+        doctor:req.body.doctor,
 
     })
     appointment1.save(function(err)
@@ -88,7 +88,7 @@ app.get('/new',function(req,res){
 
 });
 
-app.delete("/",function(req, res){
+app.delete("/deleteID",function(req, res){
     Appointment.deleteOne({ID:req.body.ID},function(err){
         if(err)console.log(err); 
         else res.send("Your appointment is succesfully cancelled")})
@@ -105,9 +105,55 @@ app.get('/appointments',function(req,res){
             {
                 array.push(appointment);
             })
+            res.json({name:array});
+            //res.send({array});
+        }
+    })
+})
+app.post('/particular',function(req,res){
+    Appointment.find(function(err,appointments)
+    {
+        var array=[]
+        if(err)console.log(err);
+        else 
+        {
+            appointments.forEach(function(appointment)
+            {
+                if(appointment.ID===req.body.ID)
+                array.push(appointment);
+            })
             //res.json(array);
             res.send(array);
         }
+    })
+
+})
+app.post('/update',function(req,res)
+{
+    Appointment.updateOne({ID:req.body.ID},{time:req.body.time},function(err,docs){
+        if(err)console.log(error);
+        else console.log(req.body.time);
+    });
+    Appointment.find(function(err,appointments)
+    {
+        //var array=[]
+        let l=0
+        if(err)console.log(err);
+        else 
+        {
+            appointments.forEach(function(appointment)
+            {
+                if(appointment.ID===req.body.ID){
+                    l=1;
+                    console.log("hi")
+                    res.send(appointment);}
+                //array.push(appointment);
+            })
+            //res.json(array);
+            // res.send(array);
+        }
+        if(l===0)res.send("Was not able to update");
+        
     })
 })
     app.listen(5000, function(){console.log("Server started on port 5000.");} )
